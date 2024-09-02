15a137eb5a01e0192b21400391163cdb20835b6e
Brian Dear <superacidjax@me.com>
2024-09-02 20:09:07 +0200

    enables v7uuid

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,10 @@ module Andorra
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.generators do |generate|
+      generate.orm :active_record, primary_key_type: :uuid
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
