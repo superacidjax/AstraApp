f70a5451317b2fed56d4afd68bfd464e49b9a24d
Brian Dear <superacidjax@me.com>
2024-09-03 17:11:26 +0200

    adds entity relationship diagram #188206956

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,7 @@
 class Goal < ApplicationRecord
   belongs_to :account
+  has_many :goal_rules
+  has_many :rules, through: :goal_rules
 
   validates :success_rate, presence: true, numericality: true
 end
--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,6 +1,6 @@
 class Rule < ApplicationRecord
   belongs_to :account
-  has_many :client_applications, through: :client_application_rules
+  has_many :goal_rules
   has_many :goals, through: :goal_rules
 
   validates :name, presence: true, uniqueness: { scope: :account_id }
