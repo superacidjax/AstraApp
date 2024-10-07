e800c430534583dda095b69703fa1f33d609f0ea
Brian Dear <superacidjax@me.com>
2024-10-07 12:43:16 +0200

    creates goals controller and initial form  (#23)

--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,16 @@ Rails.application.routes.draw do
   # health check
   get "up" => "rails/health#show", as: :rails_health_check
 
-  Rails.application.routes.draw do
-    namespace :api do
-      namespace :v1 do
-        post "people", to: "people#create"
-        post "events", to: "events#create"
-      end
+  resources :goals
+
+  namespace :api do
+    namespace :v1 do
+      post "people", to: "people#create"
+      post "events", to: "events#create"
     end
   end
-
-  # Render dynamic PWA files from app/views/pwa/*
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 end
+
+# Render dynamic PWA files from app/views/pwa/*
+# get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+# get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
