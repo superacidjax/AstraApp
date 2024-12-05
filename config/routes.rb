560eb64ecbd1a71ddce9a6c657b0510a82806bb6
Brian Dear <superacidjax@me.com>
2024-12-05 21:46:24 +0100

    fixes PWA routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,8 @@ Rails.application.routes.draw do
       post "events", to: "events#create"
     end
   end
-end
 
-# Render dynamic PWA files from app/views/pwa/*
-# get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-# get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # Render dynamic PWA files from app/views/pwa/*
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+end
